073c029fc1246489a7c0ab814878a25c43d87ea3
hayoung1214 <55429156+hayoung1214@users.noreply.github.com>
2021-03-30 13:00:21 +0900

    Create preaxiosRequest.js

---
+++ server/module/preaxiosRequest.js
@@ -0,0 +1,24 @@
+
+const FormData = require('form-data');
+const axios = require('axios')
+const fs = require('fs')
+const preaxiosRequest = async (filePath)=>{
+  var newFile = fs.createReadStream(filePath);
+  //console.log('=================')
+  //console.log(filePath)
+  //console.log('==========!!!!!!=======')
+  //console.log(newFile)
+  const formData = new FormData();
+  formData.append("file", newFile, newFile.name);
+  
+  console.log(formData)
+  try{
+    var response = await axios.create({headers: formData.getHeaders()}).post("http://.../Predict/prepare", formData)
+    console.log("3333333333333")
+    return response
+  } catch(e){
+    console.log("[ERROR|preaxiosRequest] ", e) 
+    return e
+  }
+}
+module.exports = preaxiosRequest
