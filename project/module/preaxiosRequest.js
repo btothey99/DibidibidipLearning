dc42fb24567621ded1447972d0c500799c0aeadd
hayoung1214 <55429156+hayoung1214@users.noreply.github.com>
2021-06-04 14:55:12 +0900

    Create preaxiosRequest.js

---
+++ project/module/preaxiosRequest.js
@@ -0,0 +1,19 @@
+const FormData = require('form-data');
+const axios = require('axios')
+const fs = require('fs')
+const preaxiosRequest = async (filePath)=>{
+  var newFile = fs.createReadStream(filePath);
+  const formData = new FormData();
+  formData.append("file", newFile, newFile.name);
+  
+  console.log(formData)
+  try{
+    var response = await axios.create({headers: formData.getHeaders()}).post("http://b11e9442334d.ngrok.io/Predict/prepare", formData)
+   
+    return response
+  } catch(e){
+    console.log("[ERROR|preaxiosRequest] ", e) 
+    return e
+  }
+}
+module.exports = preaxiosRequest
